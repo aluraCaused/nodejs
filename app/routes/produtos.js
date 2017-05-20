f2c2be3941278748709ff142e834a875e730b85c
Gustavo Silva <gustavo.silva@verity.com.br>
2017-05-19 21:56:50 -0300

    encapsulating connection

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,13 +1,8 @@
+var connectionFactory = require("../infra/connectionFactory");
 module.exports = function(app){
 	app.get("/produtos", function(request, response){
 
-		var mysql = require("mysql");
-		var connection = mysql.createConnection({
-			host : "localhost",
-			user : "root",
-			password: "root",
-			database:"casadocodigo_nodejs"
-		});
+		var connection = connectionFactory();
 
 		connection.query("select * from produtos", function(err, results){
 			response.render("produtos/lista", {lista:results})
