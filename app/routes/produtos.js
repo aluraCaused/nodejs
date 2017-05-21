da719baaf21b409502b99b8d19b8be7e56515b50
Gustavo Alves dos Santos Silva <gustavoalves.silva97@gmail.com>
2017-05-21 13:11:23 -0300

    isolating sql queries

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,10 +3,12 @@ module.exports = function(app){
 	app.get("/produtos", function(request, response){
 
 		var connection = app.infra.connectionFactory();
+		var produtosBanco = app.infra.produtosBanco;
 
-		connection.query("select * from produtos", function(err, results){
+		produtosBanco.lista(connection, function(err, results) {
 			response.render("produtos/lista", {lista:results})
 		});
+		
 
 		connection.end();
 
