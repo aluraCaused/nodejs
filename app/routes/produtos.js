634c06d21e6b0d7953f4faeab7fb67cd72496554
Gustavo Silva <gustavo.silva@verity.com.br>
2017-05-26 18:49:21 -0300

    saving product in db

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -18,4 +18,15 @@ module.exports = function(app){
 	app.get("/produtos/form", function(request, response){
 		response.render("produtos/form");
 	})
+
+	app.post("/produtos", function(request, response){
+		var produto = request.body;
+		
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.salva(produto, function(err, results){
+			response.redirect("/produtos");
+		})
+	})
 }
