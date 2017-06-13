49d8a8136afaa6e43f7f952a07d0ede0cb3f5fd5
Gustavo Silva <gustavo.silva@verity.com.br>
2017-06-12 21:04:57 -0300

    Adding tests to the project

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,23 @@
+var express = require("../config/express")();
+var request = require("supertest")(express);
+
 describe("#ProdutosController", function(){
-    it("#listagemJson", function(){
-        console.log("Teste");
+    it("#listagem json", function(done){
+        request.get("/produtos")
+        .set("Accept", "text/html")
+        .expect("Content-type", /html/)
+        .expect(200, done);
+    })
+
+    it("#cadastro de produtos com dados inválidos", function(done){
+        request.post("/produtos")
+        .send({titulo:"", descricao:"novo livro"})
+        .expect(400, done);
+    })
+
+    it("cadastro de produtos com dados válidos", function(done){
+        request.post("/produtos")
+        .send({titulo:"Livro teste", descricao:"novo livro", preco :20.10})
+        .expect(302, done);
     })
 })
