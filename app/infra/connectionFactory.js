4929647beb39f0c4f8eed5fdbc6de458698ed85d
Gustavo Silva <gustavo.silva@verity.com.br>
2017-06-19 09:21:49 -0300

    Preparing environment for heroku

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -18,6 +18,17 @@ function createDBConnection(){
 				database:"casadocodigo_nodejs_test"
 			});
 		}
+
+		if(process.env.NODE_ENV == "production"){
+			return mysql.createConnection({
+				host : "us-cdbr-iron-east-03.cleardb.net",
+				user : "be7597d8a485a5",
+				password: "14b3901f",
+				database:"heroku_761000acc974b51"
+			});
+		}
+
+		
 }
 
 module.exports = function(){
